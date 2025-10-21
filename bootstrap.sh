53e364add3bca3ef5f21446ab92c0486bfc0d3b7
Kevin Aud <kevinaud@gmail.com>
2025-10-21 13:53:54 -0400

    Improve bootstrap for devcontainers

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin main;
+git pull origin main
 
 function doIt() {
 	rsync --exclude ".git/" \
@@ -11,17 +11,18 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+		-avh --no-perms . ~
+	source ~/.bash_profile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+# If -f is passed OR if not running in an interactive terminal, run automatically.
+if [ "$1" == "--force" -o "$1" == "-f" ] || [ ! -t 0 ]; then
+	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		doIt
+	fi
+fi
+unset doIt
